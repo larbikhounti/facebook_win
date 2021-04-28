ea0ac434a3bc93cf65fa8b27fef6cecdad4d634e
larbikhounti <larbikhounti@gmail.com>
2021-04-27 18:26:09 -0700

    rr

---
+++ README.md
@@ -0,0 +1,86 @@
+
+# facebook win
+
+initiate live stream to facebook using obs-websocket,
+for each comment on the live video
+the scene will change causing the countDown script
+to  restart.
+
+
+
+
+## Variables needed
+
+To run this project, you will need to add the following environment variables to your .env file
+
+#### obs config
+`ADRESS = localhost:4444`
+
+`PASSWORD  = password_you_created_on_obs`
+
+
+#### facebook config
+`ACCESS_TOKEN = your_app_access_token`
+
+`VIDEO_ID = your_live_facebook_video_id`
+
+
+
+
+
+
+  
+## Installation 
+
+Install my-project with npm
+
+
+    
+
+
+
+
+  
+## steps 
+
+- add obs websocket plugin you can ge it from [obs-websocket](https://github.com/Palakis/obs-websocket/releases)
+
+- set the adress and the password you will need to add them on .env file
+
+- generate facebook app user token or user token with those permissions [public_profile,user_videos] you will need the token to add it on .env file [from here](https://developers.facebook.com/tools/explorer/) 
+
+- add count down script to obs and set time and input name (you will find it on scripts directoy)
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/larbikhounti/facebook_win.git
+```
+
+Go to the project directory
+
+```bash
+  cd facebook_win
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+  
+## Authors
+
+- [@mohamed khounti ](https://github.com/larbikhounti)
+
+  
--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const obsController = require("./obsController");
 const EventSource = require("eventsource");
+require('dotenv').config();
 // create instance of obs Controller and passing adress and password of obs socket
-const myController = new obsController("localhost:4444","admin");
+const myController = new obsController(process.env.ADRESS,process.env.PASSWORD);
+
 
 // setting token and video id
-let access_token =
-  "EAAeKlDx3HnsBAHN8NWPEE3ZCPuRpqZBhz1BnlDpJj47CRz03eSe5OATxYC59Mf7jWtwjfmqcKX5oZA3Ntzcn26XAiDmKzAOfX4iTK66FRbvMZBLrbG0cv8G7FC8Qh7nibSCmxchZA6ULAMcZCdgzPQIpbDTIjcmWJ73M7CzWAPWBBOMZASR8o51VEt0NveGRXvZCUAZAsQNtITwZDZD";
-let video_id = "390622588607928";
-let source = new EventSource(
-  "https://streaming-graph.facebook.com/" +
-    video_id +
-    "/live_comments?access_token=" +
-    access_token +
-    "&comment_rate=one_per_two_seconds&fields=from{name,id},message"
+let access_token = process.env.ACCESS_TOKEN
+let video_id = process.env.VIDEO_ID
+let source = new EventSource("https://streaming-graph.facebook.com/" +video_id+"/live_comments?access_token=" +access_token+"&comment_rate=one_per_two_seconds&fields=from{name,id},message"
 );
 
 
@@ -22,8 +18,11 @@ myController.Connect().then((state) => {
       // if stream started listen to comments
     source.onmessage = function (event) {
       console.log(event);
-      // play the stream from the start
+      // restart countDown
+      myController.switchScenes()
     };
+  }else{
+      console.log(state)
   }
 });
 
