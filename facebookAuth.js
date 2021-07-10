33c0e84cea94ee7966b1c8fad5360e7540952e63
larbikhounti <larbikhounti@gmail.com>
2021-07-10 12:12:53 +0100

    making it working with the server

--- facebookAuth.js
+++ facebookAuth.js
@@ -9,7 +9,7 @@ class facebookAuth {
   getUserpermissions() {
     const stringifiedParams = queryString.stringify({
       client_id: process.env.CLIENT_ID,
-      redirect_uri: "http://localhost:3000/authenticate/facebook/",
+      redirect_uri: "https://localhost:3000/authenticate/facebook/",
       scope: ["public_profile", "pages_manage_posts","user_videos","pages_show_list","pages_read_engagement","pages_read_user_content"].join(","), // comma seperated string
       response_type: "code",
       auth_type: "rerequest",
@@ -25,11 +25,12 @@ class facebookAuth {
     const client_id = process.env.CLIENT_ID;
     const client_secret = process.env.APP_SECRET;
     const url = new URL(
-      `https://graph.facebook.com/v4.0/oauth/access_token?redirect_uri=http://localhost:3000/authenticate/facebook/&client_id=${client_id}&client_secret=${client_secret}&code=${code}`
+      `https://graph.facebook.com/v4.0/oauth/access_token?redirect_uri=https://facebook-live-auth.herokuapp.com/authenticate/facebook/&client_id=${client_id}&client_secret=${client_secret}&code=${code}`
     );
     await fetch(url)
       .then((res) => res.json())
       .then(async (json) => {
+        console.log("json " +JSON.stringify(json) )
         if (json.access_token) {
           console.log(chalk.green("facebook access token connected."))
           // save ACCESS_TOKEN in .env
--- index.js
+++ index.js
@@ -20,13 +20,12 @@ const myController = new obsController(
   process.env.PASSWORD
 );
 
-
 // start live streaming on facebook and get post id and stream url
 async function startLiveVideoStreaming() {
   let data = {
     title: "test",
     description: "this is a test",
-    privacy: privacies.friends,
+    privacy: privacies.public,
     accessToken: process.env.ACCESS_TOKEN,
   };
   let isLiveStremingOnFacebookStrated = false;
@@ -43,6 +42,7 @@ async function startLiveVideoStreaming() {
 }
 
 app.get("/authenticate/facebook/", (req, res) => {
+  console.log(req.query.code);
   //generate access tokken
   FB.getAccessTokenFromCode(req.query.code).then(async (state) => {
     // if the state is "true" that means we got the access token
@@ -62,7 +62,7 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
   console.log(chalk.yellow("waiting for facebook permission ..."));
   // get user permissions
-  FB.getUserpermissions();
+  //FB.getUserpermissions();
 });
 
 function connectAndStartStreaming(stream_url) {
@@ -79,78 +79,82 @@ function connectAndStartStreaming(stream_url) {
   );
 
   // connecting to obs and starting the stream
-  
-  myController.Connect(stream_url).then((state) => {
-     
-    if (state) {
-      if(globalSettings.isLiveJustStared=== false){
 
-        let interval = setInterval(function async () {
-          myController.getSourceSettingsForStartingCountDown().then(res=>{
-            let content = "<html><head><meta http-equiv='refresh' content='5' /><style>body{overflow-x:hidden;overflow-y:hidden} img{height: 100vh;width: 100vw;}</style></head><body height='100vh'><img  src='./person.jpg'><script></script></body></html>"
-            if(res.sourceSettings.text === "start"){
-              // accepted words in comments 
-              const acceptedWords = [process.env.WORD_0,process.env.WORD_1,process.env.WORD_2,process.env.WORD_3];
-              for (let i = 0; i<= 3; i++) {
-                fs.writeFile(`./profile_pic/${i}.html`, content,async function () {
-                  myController.setDefaultProfilepics(i)    
-                });
-                
+  myController.Connect(stream_url).then((state) => {
+    if (state) {
+      if (globalSettings.isLiveJustStared === false) {
+        let interval = setInterval(function async() {
+          myController.getSourceSettingsForStartingCountDown().then((res) => {
+            let content =
+              "<html><head><meta http-equiv='refresh' content='5' /><style>body{overflow-x:hidden;overflow-y:hidden} img{height: 100vh;width: 100vw;}</style></head><body height='100vh'><img  src='./person.jpg'><script></script></body></html>";
+            if (res.sourceSettings.text === "start") {
+              // accepted words in comments
+              const acceptedWords = [
+                process.env.WORD_0,
+                process.env.WORD_1,
+                process.env.WORD_2,
+                process.env.WORD_3,
+              ];
+              for (let i = 0; i <= 3; i++) {
+                fs.writeFile(
+                  `./profile_pic/${i}.html`,
+                  content,
+                  async function () {
+                    myController.setDefaultProfilepics(i);
+                  }
+                );
               }
               globalSettings.isCommentsAllowed = true;
-              myController.getSourceSettingsForUsersCountDown().catch(ex=>console.log(ex))
+              myController
+                .getSourceSettingsForUsersCountDown()
+                .catch((ex) => console.log(ex));
               myController.switchToPrimary();
               source.onmessage = async function (newComment) {
-                
                 console.log("listenning to comments");
-                // if there is a new comment 
+                // if there is a new comment
                 if (newComment) {
                   // if user is allowed to comment
                   if (globalSettings.isCommentsAllowed) {
                     // myController.switchScenes() // restart countDown
                     let comment = JSON.parse(newComment.data);
                     // if the comment is allowed
-                    if(acceptedWords.includes(comment.message.toLowerCase())){
-                      
+                    if (acceptedWords.includes(comment.message.toLowerCase())) {
                       // get the user picure url
-                      let myImageResult = await getUserPicture(comment.from.id).then(
-                        (res) => res
-                      );
+                      let myImageResult = await getUserPicture(
+                        comment.from.id
+                      ).then((res) => res);
                       //console.log(myImageResult.data.url);
                       /*put await here*/ myController
-                        .downloadAndSaveit(myImageResult.data.url, comment.from.name)
+                        .downloadAndSaveit(
+                          myImageResult.data.url,
+                          comment.from.name
+                        )
                         .then(async (res) => {
-                         // myController.switchScenes();
-                         //refresh browser
-                      
-  
+                          // myController.switchScenes();
+                          //refresh browser
                         });
                     } // end of if accepted words
-
-                  } // end of if comment is allowed 
+                  } // end of if comment is allowed
                 }
               };
-              clearInterval(interval)
-              
-            }else{
-              console.log(res)
+              clearInterval(interval);
+            } else {
+              console.log(res);
             }
-          })
-          
+          });
         }, 1000);
       }
     }
   });
 }
-// if we stop the script 
-process.on('exit', function () {
+// if we stop the script
+process.on("exit", function () {
   // stop the streamig
   myController.obsStopStreaming();
 });
 
-  // catch ctrl+c event and exit normally
-process.on('SIGINT', function () {
+// catch ctrl+c event and exit normally
+process.on("SIGINT", function () {
   myController.obsStopStreaming();
-process.exit(2);
+  process.exit(2);
 });
-
